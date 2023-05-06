f17a0b4dea417e61b0727d3a2acff78c3955f089
Paulo <player_one_117@outlook.com>
2023-05-06 18:29:22 -0500

    Btn para cerrar sesion funcional

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -1,10 +1,17 @@
 <template>
   <main>
     <v-toolbar title="Practice Par Software"
-      ><v-btn icon>
+      ><v-btn @click="userStore.logout" icon>
         <v-icon>mdi-export</v-icon>
       </v-btn></v-toolbar
     >
     <slot />
   </main>
 </template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/stores/UserStore';
+
+const userStore = useUserStore();
+
+</script>
